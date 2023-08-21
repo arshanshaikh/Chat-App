003081661c33fa2728641354cbd1cb1b5bb8f651
Mohammad Arshan Shaikh <100012990+arshanshaikh@users.noreply.github.com>
2023-08-21 15:50:05 +0530

    Add files via upload

---
+++ src/firebase.jsx
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDAiSFpUFejTyQCeIlKlmqHBXV7RVnw_qY",
+  authDomain: "chatmapp-63012.firebaseapp.com",
+  projectId: "chatmapp-63012",
+  storageBucket: "chatmapp-63012.appspot.com",
+  messagingSenderId: "956954679898",
+  appId: "1:956954679898:web:e6813c542193c727a75663"
+};
+
+
+export const app = initializeApp(firebaseConfig);
